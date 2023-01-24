3da7274b0a9e76f7ad2d0201fa3acfd6d4da9f54
Zhi Li <lzblack@users.noreply.github.com>
2023-01-24 18:26:12 -0500

    Finish session 03

---
+++ session03/demo_01.py
@@ -0,0 +1,50 @@
+# print("Hello, world!")
+
+# print('Hey Jude', 'don\'t make it bad') 
+# # \ escape character
+# print("don't make it bad")
+
+# print('39 + 41 + 33 =', 39 + 41 + 33)
+
+# name = input()
+# print(name)
+
+# message = 'Hi'
+
+# n = 100 # this is a statement.
+# print(n)
+
+# # = assignment operator
+
+# 1 + 1 # an expression, it has a value.
+
+# a = 123
+# print(a)
+# a = 'ABC'
+# print(a)
+
+# x = 10
+# x = x + 2
+# print(x)
+
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(a)
+# print(b)
+
+# first_name = 'Zhi'
+# last_name = 'Li'
+# full_name = first_name + ' ' + last_name
+# print(full_name)
+
+# print('Naah, na na nanana naah, nanana naah, hey Jude. \n' * 10)
+
+
+pi = 3.1415926
+print(type(pi))
+
+print(f'Pi equals {pi:.50f}.')
+print(f'Pi equals {pi:9.5f}.')
+print(f'Pi equals {pi:8.2f}.')
---
+++ session03/demo_02.py
@@ -0,0 +1,67 @@
+# print('Hello world!')
+
+# print('Hey Jude', 'don\'t make it bad')
+# # \ escape character
+
+# print("don't make it bad")
+
+# print('39 + 41 + 33 =', 39 + 41 + 33)
+
+
+# name = input()
+# print(name)
+
+# = assignment operator
+
+# message = 'Hi'
+# variable declaration/assignment
+
+# message = None
+# message = 'Hi'
+
+# team_name = 'Philadelphia 76ers'
+
+# class = 'python'
+
+# a = 123
+# # print(type(a))
+# print(a)
+
+# a = 'ABC'
+# print(a)
+
+
+# x = 10
+# x = x + 2
+# print(x)
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b) 
+
+# first_name = "Zhi"
+# last_name = 'Li'
+# full_name = first_name + ' ' + last_name
+
+# print(full_name)
+
+# print('Naah, na na nanana naah, nanana naah, hey Jude. \n' * 10)
+
+pi = 3.1415926
+
+print(f'Pi equals {pi:.5f}.') 
+# before : -> variable
+# after : -> format
+print(f'Pi equals {pi:8.5f}.')
+print(f'Pi equals {pi:8.2f}.')
+
+# s1 = 'a'
+# s2 = 'ab'
+# s3 = 'abc'
+# s4 = 'abcd'
+
+# print(f'{s1:>10}')
+# print(f'{s2:>10}')
+# print(f'{s3:>10}')
+# print(f'{s4:>10}')
