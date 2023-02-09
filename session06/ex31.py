b22e1879459fecef9c5549c2adc398e108de6089
Zhi Li <lzblack@users.noreply.github.com>
2023-02-09 18:53:18 -0500

    Update ex31.py

--- session06/ex31.py
+++ session06/ex31.py
@@ -6,15 +6,16 @@ turtle.speed(0)
 
 def spiral(t, n):
     """
+    Ask user to enter the initial length,
     draw a spiral shape - that is drawing a square multiple times,
     after one square, turn 5 degrees
     t: a turtle
     n: int, number of squares to draw for the spiral
     """
-    # pseudo code
+    # Pseudo code
     # 1. prompt user to ask for a length value, store it into a variable
     # 2. convert variable to an integer
-    # 2. repeat the following task/steps n times
+    # 3. repeat the following task/steps n times
     #      1. draw a square with length
     #      2. turn right 5 degrees
     #      3. increase length by 4
