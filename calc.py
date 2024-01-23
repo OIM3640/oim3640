604322213adf1f41a76595441e9e85d7c6eea9fb
Zhi Li <lzblack@users.noreply.github.com>
2024-01-23 14:44:20 -0500

    Add session02 exercises solution

--- calc.py
+++ calc.py
@@ -7,8 +7,41 @@
 # print(16 // 3)
 
 
-# Exercise 02
+"""
+Ex. 2.1
+"""
+minutes = 42
+seconds = 42
 
-# Q1
+print(minutes * 60 + seconds)
 
-print(42 * 60 + 42)
+print(f"The time duration is {minutes * 60 + seconds} seconds.")
+print()
+
+"""
+Ex. 2.2
+"""
+km_per_mile = 1.61
+km = 10
+miles = km / km_per_mile
+
+print(miles)
+
+print(f"There are {miles} miles in {km} kilometers.")
+print()
+
+"""
+Ex. 2.3
+"""
+minutes = 42 + 42 / 60
+hours = minutes / 60
+
+km_per_mile = 1.61
+km = 10
+miles = km / km_per_mile
+
+pace = minutes / miles
+mph = miles / hours
+
+print("Pace in minutes per mile:", pace)
+print("Average speed in mph:", mph)
