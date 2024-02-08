8a115700ed10a959f517e2d877f93f1f09b1e294
Zhi Li <lzblack@users.noreply.github.com>
2024-02-08 16:32:50 -0500

    Finish session 08

---
+++ session08/demo_before_iteration.py
@@ -0,0 +1,46 @@
+def check(score):
+    if score >= 60:
+        print("Pass")
+        return  # immediately end this function
+    if score >= 90:
+        print("A")
+    else:
+        print("Fail")
+
+
+# check(95)
+
+
+def f(a, b, c=0):
+    # return a * 10 + b
+    return a * 100 + b * 10 + c
+
+
+# res_1 = f(3, 5, 1)  # positional arguments
+# print(res_1)
+
+# res_2 = f(b=5, a=3, c=1)  # keyword arguments
+# print(res_2)
+
+# res_3 = f(3, 5)  # c is using default value
+# print(res_3)
+
+
+def f(a, b):
+    print(a + b)
+
+
+def f2(a, b):
+    """
+    Return sum of a and b, given a, b
+
+    prompt user to enter a, b, return sum of them
+    """
+    a = int(input("Enter: "))
+    b = int(input("enter: "))
+    return a + b
+
+
+print(f2(3, 5))
+
+# print(f2())
---
+++ session08/iteration_dmeo.py
@@ -0,0 +1,74 @@
+# i = 0
+
+# while i < 4:
+#     print('Hi')
+#     i += 1
+
+# for i in range(4):
+#     print("Hi")
+
+# calculate the sum of all the integers from 0 to 100
+
+# total = 0
+
+# for i in range(101):
+#     print(f"Iteration {i}:")
+#     print(f"   before adding {i}, the current total is {total}")
+
+#     total += i
+
+#     print(f"   after adding {i}, the total becomes {total}")
+#     print()
+
+# print(total)
+
+# Create a function, sum_up_100(), that calculates the sum of all the integers from 0 to 100
+
+
+def sum_up_100():
+    total = 0
+
+    for i in range(101):
+        # print(f"Iteration {i}:")
+        # print(f"   before adding {i}, the current total is {total}")
+
+        total += i
+
+        # print(f"   after adding {i}, the total becomes {total}")
+        # print()
+
+    return total
+
+
+print(sum_up_100())
+
+# Create a function, sum_up(n), that takes an argument, n, and calculate the sum of all the integers from 0 to n
+
+
+def sum_up(n):
+    """
+    takes an argument, n, and calculate the sum of all the integers from 1 to n
+    """
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+
+    return total
+
+
+print(sum_up(100))
+
+
+def sum_up_while(n):
+    total = 0
+
+    i = 0
+
+    while i < n + 1:
+        total += i
+        i += 1
+
+    return total
+
+
+print(sum_up_while(100))
