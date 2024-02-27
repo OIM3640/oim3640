3f36e957933ce4f9dd9b9fdcbf4c2cefc9afbb09
Zhi Li <lzblack@users.noreply.github.com>
2024-02-27 16:35:03 -0500

    Finish session 12

--- session11/spelling_bee.py
+++ session11/spelling_bee.py
@@ -3,19 +3,34 @@
 from word_sol import uses_only
 
 
+def at_least(word, n):
+    """
+    Return True if the word has at least n letters
+    """
+    return len(word) >= n
+
+
+def must_use(word, letter):
+    """Return True if word contains the letter"""
+    return letter in word
+
+
 def spell_bee(must, available):
+    """
+    Print all the qualified words
+    """
     f = open("data/words.txt")
     for line in f:
         word = line.strip()
         # if the word has 4 or more letters and contains the center/required letter and uses only the available letters
-        if len(word) >= 4 and must in word and uses_only(word, available):
+        if at_least(word, 4) and must_use(word, must) and uses_only(word, available):
             print(word)
 
 
 def main():
-    must_use = "e"
-    available = "otblci"
-    spell_bee(must=must_use, available=available + must_use)
+    centered_letter = "n"
+    available = "uizped"
+    spell_bee(must=centered_letter, available=available + centered_letter)
 
 
 if __name__ == "__main__":
