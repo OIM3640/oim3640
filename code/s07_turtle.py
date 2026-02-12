36c8883b16f1e524834ca9b0ed398a34e32ea3ef
Zhi Li <lzblack@users.noreply.github.com>
2026-02-12 11:18:06 -0500

    Finish session 08

--- code/s07_turtle.py
+++ code/s07_turtle.py
@@ -2,20 +2,20 @@ import turtle
 
 
 def draw_square(turtle_obj, size=100):
-    """Draw a square with the given size."""
+    """Draw a square with the given length of side."""
     for _ in range(4):
         turtle_obj.forward(size)
         turtle_obj.left(90)
+
 
 
 def draw_spiral(t):
     """
     draw one square, turn a angle, then drawn another square and so on
     """
-    for i in range(36):
-        draw_square(t, 50)
-        t.left(10)
-    
+    for i in range(72):
+        draw_square(t, 50 + i * 2)
+        t.left(5)
 
 
 def main():
